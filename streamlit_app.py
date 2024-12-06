5b76eec099293d6963a0fea9196b3a6c2f924f1d
chrisyk59 <chrisyongkhiev@gmail.com>
2024-12-06 15:10:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,35 +19,26 @@ authenticator = Authenticate(
 )
 
 # Gérer la connexion
-name, authentication_status, username = authenticator.login("Connexion", "main")
+authenticator.login()
 
-# Définir la page d'accueil
 def accueil():
     st.title("Bienvenue chez Santa Mamamia")
 
-# Vérifier l'état d'authentification
-if authentication_status:
+if st.session_state.get("authentication_status"):
     accueil()
-    authenticator.logout("Déconnexion", "main")
+    authenticator.logout("Déconnexion")
     
     # Menu de navigation avec streamlit_option_menu
     selection = option_menu(
         menu_title=None,
-        options=["Accueil", "Photos"],
-        icons=["house", "camera"],  # Optionnel : Ajoutez des icônes
-        menu_icon="cast",  # Optionnel : Icône du menu principal
-        default_index=0,  # L'onglet par défaut
+        options=["Accueil", "Photos"]
     )
 
     if selection == "Accueil":
         st.write("Bienvenue sur la page d'accueil !")
     elif selection == "Photos":
         st.write("Bienvenue sur mon album photo")
-        # Ajoutez des photos ici si nécessaire
-        st.image(["image1.jpg", "image2.jpg"], caption=["Photo 1", "Photo 2"], width=300)
-
-elif authentication_status is False:
+elif st.session_state.get("authentication_status") is False:
     st.error("L'username ou le password est/sont incorrect(s)")
-
-elif authentication_status is None:
+elif st.session_state.get("authentication_status") is None:
     st.warning("Les champs username et mot de passe doivent être remplis")
